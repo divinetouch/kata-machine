77e7dbb1ab4a13c46312398c45f7e5b92713def2
Divine Touch <divine.touch07@gmail.com>
2023-11-19 12:42:21 -0600

    two crytal ball problems

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,22 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jmpAmount = Math.floor(Math.sqrt(breaks.length));
 
+    // jump sqrt n at a time
+    let i = jmpAmount;
+    for (; i < breaks.length; i += jmpAmount) {
+        if (breaks[i]) break;
+    }
+
+    // walk back sqrt of N
+    i -= jmpAmount;
+
+    // walk at most the sqrt of N
+    for (let j = 0; j < jmpAmount && i < breaks.length; ++j, ++i) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+
+    return -1;
 }
+
