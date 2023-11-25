045cb37aeeb7fb7296549ed574901424ce3746e8
Divine Touch <divine.touch07@gmail.com>
2023-11-25 17:55:17 -0600

    Add stack lesson

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,44 @@
+type Node<T> = {
+  value: T;
+  prev?: Node<T>;
+};
 export default class Stack<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
 
-    
+  constructor() {
+    this.head = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  push(item: T): void {
+    const node = { value: item } as Node<T>;
+
+    this.length++;
+
+    if (!this.head) {
+      this.head = node;
+      return;
     }
 
-    push(item: T): void {
+    node.prev = this.head;
+    this.head = node;
+  }
+  pop(): T | undefined {
+    this.length = Math.max(0, this.length - 1);
 
-}
-    pop(): T | undefined {
+    const head = this.head;
+    if (this.length === 0) {
+      this.head = undefined;
+      return head?.value;
+    }
 
-}
-    peek(): T | undefined {
+    this.head = head?.prev;
 
+    return head?.value;
+  }
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
-}
+
