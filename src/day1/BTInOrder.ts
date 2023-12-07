adb2141129c8c7c9aea9015a304f5a0befbf1974
Divine Touch <divine.touch07@gmail.com>
2023-12-06 20:39:05 -0600

    Add tree lessons

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,18 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+  // base case
+  if (!curr) return path;
 
+  // recurse
+
+  // recurse
+  walk(curr.left, path); // walk left
+  path.push(curr.value); // push
+  walk(curr.right, path);
+
+  // post
+  return path;
 }
+export default function in_order_search(head: BinaryNode<number>): number[] {
+  return walk(head, []);
+}
+
--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,18 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+  // base case
+  if (!curr) return path;
 
+  // recurse
+
+  // recurse
+  walk(curr.left, path); // walk left
+  walk(curr.right, path); // walk right
+
+  // post
+  path.push(curr.value); // push
+  return path;
 }
+export default function post_order_search(head: BinaryNode<number>): number[] {
+  return walk(head, []);
+}
+
