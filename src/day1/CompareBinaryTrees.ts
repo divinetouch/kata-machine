ffb026b0b05d81798a29e4323309980cc5fd14e3
Divine Touch <divine.touch07@gmail.com>
2023-12-07 21:28:35 -0600

    Compare binary tree

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,22 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+export default function compare(
+  a: BinaryNode<number> | null,
+  b: BinaryNode<number> | null,
+): boolean {
+  // structurely the same
+  if (a === null && b === null) {
+    return true;
+  }
 
+  // structurely not the same
+  if (a === null || b === null) {
+    return false;
+  }
+
+  // this is a value check
+  if (a.value !== b.value) {
+    return false;
+  }
+
+  return compare(a.left, b.left) && compare(a.right, b.right);
 }
+
